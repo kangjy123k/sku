c17bf569fc6439473fdef5bc215a3f450583c176
kangjy123k <kjy092620@gmail.com>
2019-01-01 04:26:41 +0100

    add delete and update and db field

--- database/models/Post.js
+++ database/models/Post.js
@@ -4,7 +4,12 @@ const PostSchema = new mongoose.Schema({
     location: String,
     date: Date,
     item: String,
-    description: String
+    description: String,
+    username:String,
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
 })
 
 const Post = mongoose.model('Post',PostSchema)
--- index.js
+++ index.js
@@ -2,24 +2,71 @@ const path = require('path')
 const expressEdge = require('express-edge')
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = new express()
+
+const Post = require('./database/models/Post')
 
 mongoose.connect('mongodb://localhost/lost-found')
 app.use(express.static('public'))
 app.use(expressEdge)
 app.set('views' ,`${__dirname}/views`)
 
-app.get('/',(req,resp)=>{
-    resp.render('index')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get('/',async(req,resp)=>{
+    const posts = await Post.find({})
+    console.log(posts)
+    resp.render('index',
+    {
+        posts
+    })
 })
 app.post('/posts/store',(req,resp)=>{
-    resp.redirect('/')
+    
+    Post.create(req.body,(error,post)=>{
+        console.log(error,post)
+        resp.redirect('/')
+    })
+    
 })
+app.get('/posts/delete/:id',(req,resp)=>{
+ Post.findOneAndDelete({'_id': req.params.id},
+ (error1,post)=>{
+    console.log(error1,post);
+    resp.redirect('/');
+})
+})
+app.post('/posts/update/:id',(req,resp)=>{
+    console.log(req.params.id);
+    
+    //const query = {'_id':req.params.idw};
+    // var id = req.body.result._id;s
+    // delete result._id;s
+    Post.findOneAndUpdate({'_id': req.params.id}, 
+        {$set:{
+            location: req.body.location,
+            date: req.body.date,
+            item: req.body.item,
+            description: req.body.description
+        }},
+        (error1,post)=>{
+        console.log(error1,post);
+        resp.redirect('/')
+    })
+    
+})
+
 app.get('/about',(req,resp)=>{
     resp.render('about')
 })
-app.get('/post',(req,resp)=>{
-    resp.render('post')
+app.get('/post/:id',async(req,resp)=>{
+    const post = await Post.findById(req.params.id)
+    
+    resp.render('post',{
+        post
+    })
 })
 app.get('/contact',(req,resp)=>{
     resp.render('contact')
